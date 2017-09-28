f9c5a521d6d347c32d9872c25b7e7ab2e806bd27
claudiovb <claudiocarvalhovilasboas@gmail.com>
2017-09-28 01:01:45 -0300

    Updated Ui

--- Mesa_de_Audio_UDP/mainwindow.cpp
+++ Mesa_de_Audio_UDP/mainwindow.cpp
@@ -12,3 +12,13 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_startRecord_clicked()
+{
+    ui->label->setText("gravando");
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    ui->label->setText("parou");
+}
