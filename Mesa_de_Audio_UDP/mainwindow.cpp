075f4fbb515b29628a877787ad66cbef8e04ed70
claudiovb <claudiocarvalhovilasboas@gmail.com>
2017-09-29 18:37:22 -0300

    UDPSocket

--- Mesa_de_Audio_UDP/mainwindow.cpp
+++ Mesa_de_Audio_UDP/mainwindow.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "myudp.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    MyUDP* Udp = new MyUDP();
+    Udp->HelloUDP();
 }
 
 MainWindow::~MainWindow()
